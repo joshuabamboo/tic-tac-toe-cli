dfcbf079f47f7d228bab8d1d76d98898826ef61b
joshuabamboo <joshuaowens011@gmail.com>
2015-02-02 21:12:57 -0500

    add all classes to play.rb to debug transition to OO

--- player.rb
+++ player.rb
@@ -2,26 +2,4 @@ class Player
   def initialize
     @all_moves_by_player = []
   end
-
-  def player_move #player or game
-    print_board
-    @move = gets.chomp
-    if valid_move?
-      save_move(@all_moves_by_player, @move)
-      update_board_with("X")
-      update_remaining_spaces(@move)
-    else
-      puts "'#{@move}' is not a valid move. Please enter one of the following boxes: #{@remaining_spaces}"
-      player_move
-    end
-    
-    #check_for_winner(@all_moves_by_player)
-  end
-
-  def save_move(all_moves, current_move) #player
-    all_moves << current_move.to_i
-  end
-
-  #add in choice of x or o method
-  #add in whether you go first or second
 end
